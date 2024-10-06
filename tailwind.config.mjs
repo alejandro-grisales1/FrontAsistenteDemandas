9eb5ddbb55c349ae5775e76111c0aded62c3b7d2
alejandro-grisales1 <alejandrogrisalesvelez@gmail.com>
2024-10-06 15:45:02 -0500

    pagina home v1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        fondo: "#012D5A",
+        fondo1: "#24517E",
+        fondo2: "#90ABC3",
+        fondo3: "#F7F7F7",
+        verde: "#4CAF50",
+        rojo: "#D23E3E",
+      },
+    },
+  },
+  plugins: [],
+};
